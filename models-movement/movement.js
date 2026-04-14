015ab5e9b28ee7d61e97ca8b0e8bcfbfa3cda1f4
Upserge <Upserge@gmail.com>
2026-04-14 10:16:45 -0700

    Tunneling logic and add tunnels to existing grids (#36)
    You can now use "8" anywhere in existing or new grids and that will
    create a tunnel.
    
    NOTE:
    You need to make sure the 8's are matching in row and column so that the
    teleport works correctly, and also the 8's need to be accessible
    otherwise they will be invalid.
    
    (i.e. don't put a tunnel somewhere pacman couldn't get to, like behind
    another wall)

--- models-movement/movement.js
+++ models-movement/movement.js
@@ -1,3 +1,22 @@
 export function canTravelTo(grid, row, col) {
   return grid[row]?.[col] !== 1;
 }
+
+export function getTeleportDestination(grid, row, col, direction) {
+  if (grid[row]?.[col] !== 8) return null;
+
+  if (direction.col !== 0) {
+    const pairedCol = direction.col > 0
+      ? grid[row].indexOf(8)
+      : grid[row].lastIndexOf(8);
+    if (pairedCol === col) return null;
+    return { row, col: pairedCol + direction.col };
+  }
+
+  const colValues = grid.map(r => r[col]);
+  const pairedRow = direction.row > 0
+    ? colValues.indexOf(8)
+    : colValues.lastIndexOf(8);
+  if (pairedRow === row) return null;
+  return { row: pairedRow + direction.row, col };
+}
