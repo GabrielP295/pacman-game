344799110ad1c67fdc990064ce1a4ba081d6d822
Jason Salas <upserge@gmail.com>
2026-04-14 18:40:46 -0700

    Fix bad import, create release interval

--- models-movement/ghost-release.js
+++ models-movement/ghost-release.js
@@ -1,14 +1,14 @@
-import { gV } from "./game-map/Global-Variables/global-variables.js";
-
-const RELEASE_INTERVAL_MS = 3000 / gV.level;
+const BASE_RELEASE_INTERVAL_MS = 3000;
 const RELEASE_POSITION = { row: 7, col: 9 };
 
 export function updateGhostRelease(gV, currentTime) {
   const nextGhost = gV.ghosts.find(ghost => !ghost.active);
   if (!nextGhost) return;
 
+  const releaseInterval = BASE_RELEASE_INTERVAL_MS / gV.level;
+
   gV.lastGhostRelease ??= currentTime;
-  if (currentTime - gV.lastGhostRelease < RELEASE_INTERVAL_MS) return;
+  if (currentTime - gV.lastGhostRelease < releaseInterval) return;
 
   gV.grid[nextGhost.row][nextGhost.col] = nextGhost.underlyingTile;
   nextGhost.row = RELEASE_POSITION.row;
