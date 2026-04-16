a2e60f0c1c15fc37c76ddccaf9f5ce191f36109a
pdudette151 <karim.sabrina@gmail.com>
2026-04-15 20:39:30 -0400

    cleaning up start-overlay.js to be neater

--- models-start/start-overlay.js
+++ models-start/start-overlay.js
@@ -1,21 +1,12 @@
 import { startGame } from "../pacman.js";
 
-const splash = document.getElementById("intro-splash");
-
-// Wait 2 seconds, then fade out
-setTimeout(() => {
-  splash.classList.add("fade-out");
-}, 2000);
-
-// Optional: once fully faded, remove from DOM so it can't interfere
-splash.addEventListener("transitionend", () => {
-  splash.remove();
-});
-
-//Start button overlay
 document.addEventListener("DOMContentLoaded", () => {
+  const splash = document.getElementById("intro-splash");
   const overlay = document.getElementById("start-overlay");
   const startButton = document.getElementById("start-button");
+
+  setTimeout(() => splash.classList.add("fade-out"), 2000);
+  splash.addEventListener("transitionend", () => splash.remove());
 
   startButton.addEventListener("click", () => {
     overlay.classList.add("hidden");
