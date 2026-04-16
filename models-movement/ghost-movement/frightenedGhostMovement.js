9e22067ebd5e2ffb3781b70ed655937fef27329c
Anas Al-rawi <rawi.anas@gmail.com>
2026-04-16 19:09:02 -0400

    add frightened ghost logic to update-ghost-positions.js

--- models-movement/ghost-movement/frightenedGhostMovement.js
+++ models-movement/ghost-movement/frightenedGhostMovement.js
@@ -1,10 +1,6 @@
 import { directions, isOppositeDirection } from "./ghost-utils.js";
 import { canTravelTo } from "../movement.js";
 
-function frightenedDirectionFilter(dir) {
-    return can
-}
-
 export function calculateFrightenedDirection(ghost, grid) {
   const valid = directions.filter(dir => canTravelTo(grid, ghost.row + dir.row, ghost.col + dir.col));
 
--- models-movement/handle-pacman-ghost-colision.js
+++ models-movement/handle-pacman-ghost-colision.js
@@ -1,7 +1,10 @@
-export function handlePacmanGhostCollision(grid, newPacmanPos) {
-    if (grid[newPacmanPos.row]?.[newPacmanPos.col] === 2) {
-      // handle death 
-      return true;
+export function handlePacmanGhostCollision(grid, newPacmanPos, ghosts) {
+    if (grid[newPacmanPos.row]?.[newPacmanPos.col] !== 2) {
+      return {hitGhost: false, ateGhost: false};
     }
-    return false;
+    
+    const ghost = ghosts.find(g => g.row === newPacmanPos.row && g.col === newPacmanPos.col);
+    if (ghost.frightened) return {hitGhost: false, ateGhost: true};
+
+    return {hitGhost: true, ateGhost: false};
 }
